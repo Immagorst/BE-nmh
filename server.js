c579ffd89bd7a8673f21c2788919731f657ebb8b
nmh_05 <nmh31052005179@gmail.com>
2025-01-01 13:36:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Cấu hình CORS
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://fe-nmh.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
